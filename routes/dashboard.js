b857873172426f76391a8a3b96a9b68c3f48f12b
christian <christian.cervantes@media-meter.com>
2020-08-20 18:53:22 +0800

    updated dashboard.js

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -2,12 +2,12 @@ const { dashboardCtl } = require('../controllers')
 
 module.exports = async (name, auth, router) => {
 
-    router.get(name+'/website_status', await dashboardCtl.WEBSITE_STATUS)
+    router.get(name+'/website_status', auth, await dashboardCtl.WEBSITE_STATUS)
 
-    router.get(name+'/website_category', await dashboardCtl.WEBSITE_CATEGORY)
+    router.get(name+'/website_category', auth, await dashboardCtl.WEBSITE_CATEGORY)
 
-    router.get(name+'/website_type', await dashboardCtl.WEBSITE_TYPE)
+    router.get(name+'/website_type', auth, await dashboardCtl.WEBSITE_TYPE)
 
-    router.get(name+'/article_status', await dashboardCtl.ARTICLE_STATUS)
+    router.get(name+'/article_status', auth, await dashboardCtl.ARTICLE_STATUS)
 
 }
