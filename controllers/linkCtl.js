96fd2bc5d50c72243abbc1062e3ed0f16fd5408f
christian <christian.cervantes@media-meter.com>
2020-10-01 16:27:03 +0800

    updated linkCtl.js

--- controllers/linkCtl.js
+++ controllers/linkCtl.js
@@ -49,7 +49,7 @@ link.CUSTOM_QUERY = async (req, res, next) => {
         sorting[sort] = parseInt(sortBy)
         // console.log(sorting)
         let filter = req.body || {}
-        const result = await link.find(filter, fields).populate('website', '-embedded_sections -main_sections -sub_sections').limit(parseInt(limit)).skip(parseInt(offset)).sort(sorting)
+        const result = await links.find(filter, fields).populate('website', '-embedded_sections -main_sections -sub_sections').limit(parseInt(limit)).skip(parseInt(offset)).sort(sorting)
         res.status(200).send({'data': result})
     } catch (error) {
         next(createError(error))
