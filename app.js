881fceca385dbec5567bff5f8d6375018dc51ca0
christian <christian.cervantes@media-meter.com>
2020-10-12 16:22:49 +0800

    adjust request entity too large

--- app.js
+++ app.js
@@ -36,8 +36,8 @@ mongoose.connection.on('error', function (err){
 
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({limit: '500mb'}));
+app.use(express.urlencoded({ limit: '500mb', extended: true }));
 app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, 'public')));
 
