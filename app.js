7d46eace212b5374977bb6a79231205190c59a11
Christian S. Cervantes <christian.cervantes@media-meter.com>
2021-08-28 03:25:07 +0800

    updated remove cors

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 // var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var cors = require('cors')
+// var cors = require('cors')
 
 var { mongodb } = require('./helpers/setting')
 
@@ -36,7 +36,7 @@ mongoose.connection.on('error', function (err){
 // app.set('view engine', 'pug');
 
 
-app.use(cors())
+// app.use(cors())
 app.use(logger('dev'));
 app.use(express.json({limit: '500mb'}));
 app.use(express.urlencoded({ limit: '500mb', extended: true }));
