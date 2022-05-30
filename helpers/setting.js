c295415c43afb8b23d6a242ba7bea055bc291206
Christian S. Cervantes <christian.cervantes@media-meter.com>
2022-05-30 14:34:27 +0800

    updated

--- helpers/setting.js
+++ helpers/setting.js
@@ -16,10 +16,18 @@ module.exports = {
         connectTimeout: 59000
     },
     mongodb : {
+        // production: {
+        //     host: 'mongo1', // primary
+        //     host2: 'mongo2', // secondary
+        //     host3: 'mongo3', // secondary
+        //     user: 'mongo-admin',
+        //     db: 'mmi_scraper_2020',
+        //     pass: 'Ew5mj4B5^sie136A423n7519H',
+        // },
         production: {
-            host: 'mongo1', // primary
-            host2: 'mongo2', // secondary
-            host3: 'mongo3', // secondary
+            host: 'mongodb1', // primary
+            host2: 'mongodb2', // secondary
+            host3: 'mongodb3', // secondary
             user: 'mongo-admin',
             db: 'mmi_scraper_2020',
             pass: 'Ew5mj4B5^sie136A423n7519H',
@@ -28,20 +36,29 @@ module.exports = {
             host: 'localhost:27017',
             db: 'mmi_scraper_2020'
         },
+        // options: {
+        //     useUnifiedTopology: true,
+        //     useNewUrlParser: true,
+        //     useCreateIndex: true,
+        //     useFindAndModify: false,
+        //     autoIndex: false, // Don't build indexes
+        //     // reconnectTries: Number.MAX_VALUE, // Never stop trying to reconnect
+        //     // reconnectInterval: 500, // Reconnect every 500ms
+        //     poolSize: 100000, // Maintain up to 10 socket connections
+        //     // If not connected, return errors immediately rather than waiting for reconnect
+        //     bufferMaxEntries: 0,
+        //     // connectTimeoutMS: 10000, // Give up initial connection after 10 seconds
+        //     connectTimeoutMS: 59000, // Give up initial connection after 10 seconds
+        //     // socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+        //     socketTimeoutMS: 59000, // Close sockets after 45 seconds of inactivity
+        //     family: 4 // Use IPv4, skip trying IPv6
+        // }
         options: {
             useUnifiedTopology: true,
             useNewUrlParser: true,
-            useCreateIndex: true,
-            useFindAndModify: false,
-            autoIndex: false, // Don't build indexes
-            // reconnectTries: Number.MAX_VALUE, // Never stop trying to reconnect
-            // reconnectInterval: 500, // Reconnect every 500ms
-            poolSize: 100000, // Maintain up to 10 socket connections
-            // If not connected, return errors immediately rather than waiting for reconnect
-            bufferMaxEntries: 0,
-            // connectTimeoutMS: 10000, // Give up initial connection after 10 seconds
+            autoIndex: true, // Don't build indexes
+            maxPoolSize: 10000, // Maintain up to 10 socket connections
             connectTimeoutMS: 59000, // Give up initial connection after 10 seconds
-            // socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
             socketTimeoutMS: 59000, // Close sockets after 45 seconds of inactivity
             family: 4 // Use IPv4, skip trying IPv6
         }
